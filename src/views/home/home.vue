35daa130b09e4b977f6bcd4d6fcbcd37d8ace5e5
unknown <2735720689@qq.com>
2021-08-04 10:12:13 +0800

    '2'

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" />
+    <home-recommend-view :recommends="recommends" />
   </div>
 </template>
 <script>
 import NavBar from "../../components/common/navbar/NavBar.vue";
+import HomeSwiper from "./childrencomponents/Homeswiper.vue";
+import HomeRecommendView from "./childrencomponents/HomeRecommendView.vue";
+
 import { getHomeMutlidata } from "../../network/home";
 
 export default {
   name: "home",
   components: {
     NavBar,
+    HomeSwiper,
+    HomeRecommendView,
   },
   data() {
     return {
